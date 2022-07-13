aa74ff4ee67b7b96c82dde2391d19cff9b76bb77
Alex Thalhammer <alex@angular-software.at>
2022-07-13 12:09:39 +0200

    [NxDDD] Bonus: Route Between Feature Libs

--- apps/luggage/src/app/app.module.ts
+++ apps/luggage/src/app/app.module.ts
@@ -1,15 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { LuggageFeatureCheckinModule } from '@flight-workspace/luggage/feature-checkin';
-import { HttpClientModule } from '@angular/common/http';
-import { LuggageFeatureReportLossModule } from '@flight-workspace/luggage/feature-report-loss';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, LuggageFeatureCheckinModule, HttpClientModule, LuggageFeatureReportLossModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+
+    // LuggageFeatureCheckinModule,
+    // LuggageFeatureReportLossModule,
+
+    RouterModule.forRoot([
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'check-in'
+      },
+      {
+        path: 'check-in',
+        loadChildren: () => import('@flight-workspace/luggage/feature-checkin').then((m) => m.LuggageFeatureCheckinModule)
+      },
+      {
+        path: 'report-loss',
+        loadChildren: () => import('@flight-workspace/luggage/feature-report-loss').then((m) => m.LuggageFeatureReportLossModule)
+      }
+    ])
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
+++ libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { LuggageDomainModule } from '@flight-workspace/luggage/domain';
 import { CheckinComponent } from './checkin.component';
 
 @NgModule({
-  imports: [CommonModule, LuggageDomainModule],
+  imports: [CommonModule, LuggageDomainModule, RouterModule.forChild([{ path: '', component: CheckinComponent }])],
   declarations: [CheckinComponent],
   exports: [CheckinComponent]
 })
