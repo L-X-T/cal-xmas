2af1f06d6f227f13f51c1ba14e4731b3f5647f32
Alex Thalhammer <alex@angular-software.at>
2022-07-14 09:59:40 +0200

    [RxJS] 2. Unsubscribe with takeUntil Subject

--- apps/flight-app/src/app/flight-booking/airport/airport.component.ts
+++ apps/flight-app/src/app/flight-booking/airport/airport.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AirportService } from '@flight-workspace/flight-lib';
-import { Observable, Observer, Subscription } from 'rxjs';
+import { Observable, Observer, Subject, Subscription } from 'rxjs';
 import { HttpErrorResponse } from '@angular/common/http';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'airport',
@@ -14,6 +15,11 @@ export class AirportComponent implements OnInit, OnDestroy {
   airports: string[] = [];
   private airportsObserver: Observer<string[]>;
   private airportsSubscription: Subscription;
+
+  // 2 takeUntil & subject
+  airportsTakeUntil: string[] = [];
+  private terminatorSubject = new Subject<void>();
+  readonly terminator$ = this.terminatorSubject.asObservable();
 
   constructor(private airportService: AirportService) {}
 
@@ -30,11 +36,29 @@ export class AirportComponent implements OnInit, OnDestroy {
 
     // 1 using subscription & unsubscribe
     this.airportsSubscription = this.airports$.subscribe(this.airportsObserver);
+
+    // 2 takeUntil & subject
+    this.airports$.pipe(takeUntil(this.terminator$)).subscribe({
+      next: (airports) => {
+        console.log('airportsTakeUntil$ next: ', airports);
+        this.airportsTakeUntil = airports;
+      },
+      error: (err) => {
+        console.error('airportsTakeUntil$ error: ', err);
+      },
+      complete: () => {
+        console.warn('airportsTakeUntil$ completed');
+      }
+    });
   }
 
   ngOnDestroy(): void {
     // 1 using subscription & unsubscribe
     this.airportsSubscription?.unsubscribe();
+
+    // 2 takeUntil & subject
+    this.terminatorSubject.next();
+    this.terminatorSubject.complete();
   }
 
   private onLoadAirportsSuccessfully(airports: string[]): void {
