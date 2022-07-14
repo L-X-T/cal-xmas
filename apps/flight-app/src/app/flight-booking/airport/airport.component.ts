e25b24ca54c7cd52212f93a14bc3ec64ab7cfef0
Alex Thalhammer <alex@angular-software.at>
2022-07-14 10:24:18 +0200

    [RxJS] Bonus: Add basic Error Handling

--- apps/flight-app/src/app/flight-booking/airport/airport.component.ts
+++ apps/flight-app/src/app/flight-booking/airport/airport.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AirportService } from '@flight-workspace/flight-lib';
-import { Observable, Observer, Subject, Subscription } from 'rxjs';
+import { Observable, Observer, of, Subject, Subscription } from 'rxjs';
 import { HttpErrorResponse } from '@angular/common/http';
-import { delay, share, takeUntil } from 'rxjs/operators';
+import { catchError, delay, share, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'airport',
@@ -14,19 +14,32 @@ export class AirportComponent implements OnInit, OnDestroy {
   // 1 using subscription & unsubscribe
   airports: string[] = [];
   airportsIsLoading = true;
+  airportsErrorMessage = '';
   private airportsObserver: Observer<string[]>;
   private airportsSubscription: Subscription;
 
   // 2 takeUntil & subject
   airportsTakeUntil: string[] = [];
   airportsTakeUntilIsLoading = true;
+  airportsTakeUntilErrorMessage = '';
   private terminatorSubject = new Subject<void>();
   readonly terminator$ = this.terminatorSubject.asObservable();
+
+  // 3 async airports error handling
+  asyncAirports$: Observable<string[]>;
+  asyncAirportsErrorMessage = '';
 
   constructor(private airportService: AirportService) {}
 
   ngOnInit(): void {
     this.airports$ = this.airportService.findAll().pipe(delay(3000), share());
+
+    this.asyncAirports$ = this.airports$.pipe(
+      catchError((err) => {
+        this.asyncAirportsErrorMessage = err.message;
+        return of([]);
+      })
+    );
 
     this.airportsObserver = {
       next: (airports) => this.onLoadAirportsSuccessfully(airports),
@@ -46,9 +59,10 @@ export class AirportComponent implements OnInit, OnDestroy {
         this.airportsTakeUntil = airports;
         this.airportsTakeUntilIsLoading = false;
       },
-      error: (err) => {
+      error: (err: HttpErrorResponse) => {
         console.error('airportsTakeUntil$ error: ', err);
         this.airportsTakeUntilIsLoading = false;
+        this.airportsTakeUntilErrorMessage = err.message;
       },
       complete: () => {
         console.warn('airportsTakeUntil$ completed');
@@ -74,5 +88,6 @@ export class AirportComponent implements OnInit, OnDestroy {
   private onLoadAirportsFail(err: HttpErrorResponse): void {
     console.error('airports$ error: ' + err);
     this.airportsIsLoading = false;
+    this.airportsErrorMessage = err.message;
   }
 }
