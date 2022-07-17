8ac9b6f150618935bed7b9345e38305691afeab9
Alex Thalhammer <alex@angular-software.at>
2022-07-17 21:20:30 +0200

    [unit] Mocking HTTP calls

--- apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.spec.ts
+++ apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { FlightSearchComponent } from './flight-search.component';
@@ -25,5 +25,26 @@ describe('Unit test: flight-search.component', () => {
 
   it('should not have any flights loaded initially', () => {
     expect(component.flights.length).toBe(0);
+  });
+
+  it('should load flights when user entered from and to', () => {
+    component.from = 'Graz';
+    component.to = 'Hamburg';
+    component.search();
+
+    const httpTestingController: HttpTestingController = TestBed.inject(HttpTestingController);
+    const req = httpTestingController.expectOne('http://www.angular.at/api/flight?from=Graz&to=Hamburg');
+    // req.request.method === 'GET'
+
+    req.flush([
+      {
+        id: 22,
+        from: 'Graz',
+        to: 'Hamburg',
+        date: ''
+      }
+    ]);
+
+    expect(component.flights.length).toBe(1);
   });
 });
