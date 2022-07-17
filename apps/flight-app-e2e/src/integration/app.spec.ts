b3de9352a6d52a22e5342762739330da867c7de7
Alex Thalhammer <alex@angular-software.at>
2022-07-17 22:31:51 +0200

    [e2e] Create some tests for your app

--- apps/flight-app-e2e/src/integration/app.spec.ts
+++ apps/flight-app-e2e/src/integration/app.spec.ts
@@ -21,4 +21,23 @@ describe('flight-app', () => {
         expect(measure.duration).to.be.most(1000);
       });
   });
+
+  it('should have UTF-8 as charset', () => {
+    cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
+  });
+
+  it('should do an implicit subject assertion', () => {
+    cy.get('.sidebar-wrapper ul.nav li:last a').should('contain.text', 'Basket');
+  });
+
+  it('should do an explicit subject assertion', () => {
+    cy.get('.sidebar-wrapper ul.nav li:nth-child(2) a').should(($a) => {
+      expect($a).to.contain.text('Flights');
+      expect($a).to.have.attr('href', '/flight-booking/flight-search');
+    });
+  });
+
+  it('should count the nav entries', () => {
+    cy.get('.sidebar-wrapper ul.nav li').its('length').should('be.gte', 3);
+  });
 });
