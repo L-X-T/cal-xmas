bf9320dc495f69c248beeb224685f021bdf2b2c1
Alex Thalhammer <alex@angular-software.at>
2022-12-19 17:10:29 +0100

    [Comp] Content Projection

---
+++ apps/flight-app/src/app/shared/controls/tabbed-pane/tabbed-pane.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'flight-workspace-tabbed-pane',
+  templateUrl: './tabbed-pane.component.html',
+  styleUrls: ['./tabbed-pane.component.css']
+})
+export class TabbedPaneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- apps/flight-app/src/app/shared/shared.module.ts
+++ apps/flight-app/src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CityPipe } from './pipes/city.pipe';
+import { TabbedPaneComponent } from './controls/tabbed-pane/tabbed-pane.component';
+import { TabComponent } from './controls/tab/tab.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [CityPipe],
-  exports: [CityPipe]
+  declarations: [CityPipe, TabbedPaneComponent, TabComponent],
+  exports: [CityPipe, TabbedPaneComponent, TabComponent]
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders<SharedModule> {
