2d6d7cf1f12eed537160076c5dbfe9fe8d2997fd
Alex Thalhammer <alex@angular-software.at>
2022-12-19 14:07:42 +0100

    Fix e2e test

--- apps/flight-app-e2e/src/integration/app.spec.ts
+++ apps/flight-app-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('flight-app', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to flight-app!');
+    getGreeting().contains('Welcome');
   });
 });
