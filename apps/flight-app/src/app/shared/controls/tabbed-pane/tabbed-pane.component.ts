c9ddd691c8093c8eeff01d5cd95b2d0752eed267
Alex Thalhammer <alex@angular-software.at>
2022-12-19 17:20:00 +0100

    [Comp] Interacting with a Component's Content

--- apps/flight-app/src/app/shared/controls/tabbed-pane/tabbed-pane.component.ts
+++ apps/flight-app/src/app/shared/controls/tabbed-pane/tabbed-pane.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, Component } from '@angular/core';
+import { AfterContentInit, Component, ContentChildren, QueryList } from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
 
 @Component({
@@ -7,17 +7,20 @@ import { TabComponent } from '../tab/tab.component';
   styleUrls: ['./tabbed-pane.component.css']
 })
 export class TabbedPaneComponent implements AfterContentInit {
-  tabs: TabComponent[] = [];
+  @ContentChildren(TabComponent)
+  tabQueryList: QueryList<TabComponent> | undefined;
+
   activeTab: TabComponent | undefined;
+  currentPage = 1;
+
+  get tabs(): TabComponent[] {
+    return this.tabQueryList?.toArray() ?? [];
+  }
 
   ngAfterContentInit(): void {
     if (this.tabs.length > 0) {
       this.activate(this.tabs[0]);
     }
-  }
-
-  register(tab: TabComponent): void {
-    this.tabs.push(tab);
   }
 
   activate(active: TabComponent): void {
