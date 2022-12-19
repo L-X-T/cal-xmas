486bc9f74c53dc5fb21984000a1732058aedfffe
Alex Thalhammer <alex@angular-software.at>
2022-12-19 17:15:10 +0100

    [Comp] Referencing the Parent Component

--- apps/flight-app/src/app/shared/controls/tabbed-pane/tabbed-pane.component.ts
+++ apps/flight-app/src/app/shared/controls/tabbed-pane/tabbed-pane.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
-  selector: 'flight-workspace-tabbed-pane',
+  selector: 'app-tabbed-pane',
   templateUrl: './tabbed-pane.component.html',
   styleUrls: ['./tabbed-pane.component.css']
 })
-export class TabbedPaneComponent implements OnInit {
+export class TabbedPaneComponent implements AfterContentInit {
+  tabs: TabComponent[] = [];
+  activeTab: TabComponent | undefined;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngAfterContentInit(): void {
+    if (this.tabs.length > 0) {
+      this.activate(this.tabs[0]);
+    }
   }
 
+  register(tab: TabComponent): void {
+    this.tabs.push(tab);
+  }
+
+  activate(active: TabComponent): void {
+    for (const tab of this.tabs) {
+      tab.visible = tab === active;
+    }
+    this.activeTab = active;
+  }
 }
