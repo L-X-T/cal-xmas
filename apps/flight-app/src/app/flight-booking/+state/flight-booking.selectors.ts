L-X-T/buhlergroup

[NgRx] Selectors: Adding a first selector

apps/flight-app/src/app/flight-booking/+state/flight-booking.selectors.ts
@@ -1,6 +1,14 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import * as fromFlightBooking from './flight-booking.reducer';
+import { FlightBookingAppState } from './flight-booking.reducer';
 
-export const selectFlightBookingState = createFeatureSelector<fromFlightBooking.State>(
+/*export const selectFlightBookingState = createFeatureSelector<fromFlightBooking.State>(
   fromFlightBooking.flightBookingFeatureKey
+);*/
+
+export const selectFlights = (s: FlightBookingAppState) => s.flightBooking.flights;
+export const negativeList = (s: FlightBookingAppState) => s.flightBooking.negativeList;
+
+export const selectedFilteredFlights = createSelector(selectFlights, negativeList, (flights, negativeList) =>
+  flights.filter((f) => !negativeList.includes(f.id))
 );

apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
@@ -6,6 +6,7 @@ import { Store } from '@ngrx/store';
 import { FlightBookingAppState } from '../+state/flight-booking.reducer';
 import { flightsLoaded, updateFlight } from '../+state/flight-booking.actions';
 import { take } from 'rxjs/operators';
+import { selectedFilteredFlights } from '../+state/flight-booking.selectors';
 
 @Component({
   selector: 'flight-search',
@@ -22,7 +23,7 @@ export class FlightSearchComponent implements OnInit {
     5: true
   };
 
-  flights$ = this.store.select((s) => s.flightBooking.flights);
+  flights$ = this.store.select(selectedFilteredFlights);
 
   constructor(private flightService: FlightService, private store: Store<FlightBookingAppState>) {}
 

L-X-T/cal-xmas

apps/flight-app/src/app/flight-booking/+state/flight-booking.selectors.ts
@@ -1,6 +1,14 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import * as fromFlightBooking from './flight-booking.reducer';
+import { FlightBookingAppState } from './flight-booking.reducer';
 
-export const selectFlightBookingState = createFeatureSelector<fromFlightBooking.State>(
+/*export const selectFlightBookingState = createFeatureSelector<fromFlightBooking.State>(
   fromFlightBooking.flightBookingFeatureKey
+);*/
+
+export const selectFlights = (s: FlightBookingAppState) => s.flightBooking.flights;
+export const negativeList = (s: FlightBookingAppState) => s.flightBooking.negativeList;
+
+export const selectFilteredFlights = createSelector(selectFlights, negativeList, (flights, negativeList) =>
+  flights.filter((f) => !negativeList.includes(f.id))
 );

apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
@@ -6,6 +6,7 @@ import { Store } from '@ngrx/store';
 import { FlightBookingAppState } from '../+state/flight-booking.reducer';
 import { flightsLoaded, updateFlight } from '../+state/flight-booking.actions';
 import { take } from 'rxjs/operators';
+import { selectFilteredFlights } from '../+state/flight-booking.selectors';
 
 @Component({
   selector: 'flight-search',
@@ -22,7 +23,7 @@ export class FlightSearchComponent implements OnInit {
     5: true
   };
 
-  flights$ = this.store.select((s) => s.flightBooking.flights);
+  flights$ = this.store.select(selectFilteredFlights);
 
   constructor(private flightService: FlightService, private store: Store<FlightBookingAppState>) {}
 